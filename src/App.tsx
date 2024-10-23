064a51447eaa81086fa148007d6784ae829ec281
jsaurabh334 <jsaurabh334@gmail.com>
2024-10-23 15:47:55 +0530

    final

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-cyan-700'>
+      <h1>hellow wolrd</h1>
+      <Button>click me</Button>
+    </div>
+  )
+}
+
+export default App
